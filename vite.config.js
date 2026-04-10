e57ce97f578dff944da008a4e238358eaf73813c
nikoyourcousin <118354950+nikoyourcousin@users.noreply.github.com>
2026-04-10 09:42:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/repository-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/personal-kanban-board/' : '/',
   plugins: [vue()],
   server: {
     port: 3000,
